f05a9cd37cf4d58f7b8a6a87e55d423b284f4866
MrBalance <17865933616@139.com>
2019-08-12 14:28:32 +0800

    添加注释

--- springboot-dubbo-consumer/src/main/java/com/balance/SpringbootDubboConsumerApplication.java
+++ springboot-dubbo-consumer/src/main/java/com/balance/SpringbootDubboConsumerApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 启动dubbo注解
 @EnableDubboConfiguration
 @SpringBootApplication
 public class SpringbootDubboConsumerApplication {
--- springboot-dubbo-service/src/main/java/com/balance/SpringbootDubboServiceApplication.java
+++ springboot-dubbo-service/src/main/java/com/balance/SpringbootDubboServiceApplication.java
@@ -5,8 +5,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// 启动dubbo注解
 @EnableDubboConfiguration
+// dao层扫描
 @MapperScan(basePackages = "com.balance.dao")
 @SpringBootApplication
 public class SpringbootDubboServiceApplication {
